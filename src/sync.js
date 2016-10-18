f89190db6353c34dfa56ed3abdc51f57df140adb
michael <4apaev@gmail.com>
2016-10-18 19:36:33 +0300

    dont parse result, since we got new "total" field

--- src/sync.js
+++ src/sync.js
@@ -1,16 +1,13 @@
 
 const json = x => x.json()
-const parse = x => x.result
-
 exports.query = body => fetch('/characters', {
     body: JSON.stringify(body, 0, 2),
     method:'POST',
     headers: { 'Accept': 'application/json', 'Content-Type': 'application/json' }
   })
-  .then(json)
-  .then(parse)
+  .then(json);
 
-exports.find = id => fetch('/characters/' + id).then(json)
+exports.find = id => fetch('/characters/' + id).then(json);
 
 
 
